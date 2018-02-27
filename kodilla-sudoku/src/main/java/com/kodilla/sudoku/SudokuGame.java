02526a9b38ad70114d58955a6221efb6410fa9b1
KrzyzanowskaK <katkrzznwska@gmail.com>
2018-02-27 16:52:34 +0100

    12.7 sudoku

--- kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGame.java
+++ kodilla-sudoku/src/main/java/com/kodilla/sudoku/SudokuGame.java
@@ -1,4 +1,12 @@
 package com.kodilla.sudoku;
 
 public class SudokuGame {
+    public static void main(String[] args) {
+        boolean gameFinished = false;
+
+        while(!gameFinished) {
+            ResolvingSudoku theGame = new ResolvingSudoku();
+            gameFinished = theGame.resolveSudoku();
+        }
+    }
 }
