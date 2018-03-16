d03d67f4f1990ee59bd90240a4b48a503a7137ce
KrzyzanowskaK <katkrzznwska@gmail.com>
2018-03-16 01:23:55 +0100

    20.4 obserwator

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/observer/homework/MentorTestSuite.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class HomeworkTestSuite {
+public class MentorTestSuite {
 
     @Test
     public void testUpdate() {
